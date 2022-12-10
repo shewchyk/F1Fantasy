37a86738c2daa2d7fa6c73d562d565645d81135b
shewchyk <shewchykj@gmail.com>
2022-12-10 15:28:54 +0100

    reserve driversfunc shortened

--- f1fantasy.py
+++ f1fantasy.py
@@ -1,6 +1,6 @@
 # Phase 1 - Program will determine recommended fantasy moves based on a point system from a total of FP performance for standard weekends
 # Phase 2 - Include update to account for sprint weekends
-# Phase 3 - errors for entering duplicate and non-existant finishing positions
+# Phase 3 - error~s for entering duplicate and non-existant finishing positions
 
 from driverinfo import drivers, sprint_wk
 
@@ -11,21 +11,29 @@ def dr_fp1(drivers, index):
     
 #checks to see if there was a reserve driver in any of the FP sessions (0 value in "drivers" denotes reserve driver)
 #don't call on Sprint weekends
-def reserve(list, index):
-    avg = int(round((list[index][2] + list[index][3] + list[index][4]) / 3, 0))
-    for index in list:
-        if index[2] == 0:
-            index[2] = avg
+def reserve(list, driver_index, fp_index):
+    avg = int(round((list[driver_index][2] + list[driver_index][3] + list[driver_index][4]) / 3, 0))
+    random = 0
+    while random < len(list):
+        for driver_index in list:
+            if driver_index[fp_index] == 0:
+                driver_index[fp_index] = avg
+            else: pass
+        random += 1
+        fp_index += 1
+
+"""         
+            
         elif index[3] == 0:
               index[3] = avg
         elif index[4] == 0:
               index[4] = avg
         else: pass
 
-drivers_names = dr_fp1(drivers, 0)
+ """
 
 if sprint_wk != "yes":
-    reserve(drivers, 0)
+    reserve(drivers, 0, 2)
     points_fp1 = dr_fp1(drivers, 2)
     points_fp2 = dr_fp1(drivers, 3)
     points_fp3 = dr_fp1(drivers, 4)
@@ -33,6 +41,8 @@ if sprint_wk != "yes":
 else: 
     points_fp1 = dr_fp1(drivers, 2)
     sumindex = points_fp1
+
+drivers_names = dr_fp1(drivers, 0)
 
 for d, s in zip(drivers_names, sumindex):
     print("Driver/points:", s, d)
