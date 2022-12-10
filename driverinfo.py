50cf46c539f07bfd12b0822888b20e9d6d113be2
shewchyk <shewchykj@gmail.com>
2022-12-10 07:58:52 +0100

    Sprint update

--- driverinfo.py
+++ driverinfo.py
@@ -1,17 +1,33 @@
 #Name, car number
 drivers = [["Verstappen", 1], ["Ricciardo", 3],["Norris", 4], ["Vettle", 5], ["Latifi", 6], ["Gasly", 10], ["Perez", 11], ["Alonso", 14], ["Leclerc", 16], ["Stroll", 18], ["Magnussen", 20], ["Tsunoda", 22], ["Albon", 23], ["Zhou", 24], ["Ocon", 31],["Hamilton", 44], ["Schumacher", 47], ["Sainz", 55], ["Russell", 63], ["Bottas", 77]]
 
+sprint_wk = input("Is it a sprint format weekend?: ")
+
 #input FP finishing positions, extend to drivers' list
 def fp_points(driver_index, list):
     print("Enter FP finishing positions for", str(list[driver_index][0]))
     fp1, fp2, fp3 = [int(x) for x in input().split()]
     fp_point_by_session = list[driver_index].extend([fp1, fp2, fp3])
     return fp_point_by_session
- 
-#p = index of driver list
-p = 0       
-while p < len(drivers):
-    fp_points(p, drivers)
-    p += 1
+
+#Sprint weekend function
+def fp_sprint(driver_index, list):
+    print("Enter FP finishing positions for", str(list[driver_index][0]))
+    fp1 = [int(x) for x in input().split()]
+    fp_point_by_session = list[driver_index].extend([fp1]) 
+    return fp_point_by_session
+
+#p/s = index of driver list
+
+if sprint_wk == "yes":
+    s = 0
+    while s < len(drivers):
+     fp_sprint(s, drivers)
+     s += 1
+else:
+     p = 0       
+     while p < len(drivers):
+      fp_points(p, drivers)
+      p += 1
 
 #constructors = [["Alfa Romeo"], ["Alpha Tauri"], ["Alpine"], ["Aston Martin"], ["Ferrari"], ["Haas"], ["McLaren"], ["Mercedes"], ["Red Bull"], ["Williams"]]
--- f1fantasy.py
+++ f1fantasy.py
@@ -2,7 +2,7 @@
 # Phase 2 - Include update to account for sprint weekends
 # Phase 3 - errors for entering duplicate and non-existant finishing positions
 
-from driverinfo import drivers
+from driverinfo import drivers, sprint_wk
 
 drivers      
 
@@ -10,6 +10,7 @@ def dr_fp1(drivers, index):
     return [i[index] for i in drivers]
     
 #checks to see if there was a reserve driver in any of the FP sessions (0 value in "drivers" denotes reserve driver)
+#don't call on Sprint weekends
 def reserve(list, index):
     avg = int(round((list[index][2] + list[index][3] + list[index][4]) / 3, 0))
     for index in list:
@@ -21,15 +22,15 @@ def reserve(list, index):
               index[4] = avg
         else: pass
 
-reserve(drivers, 0)
-
-# convert func to int
 drivers_names = dr_fp1(drivers, 0)
 points_fp1 = dr_fp1(drivers, 2)
-points_fp2 = dr_fp1(drivers, 3)
-points_fp3 = dr_fp1(drivers, 4)
 
-sumindex = [sum(fp_total) for fp_total in zip(points_fp1, points_fp2, points_fp3)]
+if sprint_wk != "yes":
+    reserve(drivers, 0)
+    points_fp2 = dr_fp1(drivers, 3)
+    points_fp3 = dr_fp1(drivers, 4)
+    sumindex = [sum(fp_total) for fp_total in zip(points_fp1, points_fp2, points_fp3)]
+else: sumindex = points_fp1
 
 for d, s in zip(drivers_names, sumindex):
     print("Driver/points:", s, d)
