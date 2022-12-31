aee700667fca3e8ee8c992d8e7df30c8b129e668
shewchyk <shewchykj@gmail.com>
2022-12-31 16:26:54 +0100

    Reserve driver and 2023 driver updates

--- f1fantasy.py
+++ f1fantasy.py
@@ -1,41 +1,24 @@
 # Phase 1 - Program will determine recommended fantasy moves based on a point system from a total of FP performance for standard weekends
 # Phase 2 - Include update to account for sprint weekends
-# Phase 3 - error~s for entering duplicate and non-existant finishing positions
+# Phase 3 - errors for entering duplicate and non-existant finishing positions
 
-from driverinfo import drivers, sprint_wk
+from driverinfo import drivers_2023, sprint_wk
 
-drivers      
+drivers_2023 
 
 def dr_fp1(drivers, index):
-    return [i[index] for i in drivers]
-    
-#checks to see if there was a reserve driver in any of the FP sessions (0 value in "drivers" denotes reserve driver)
-#don't call on Sprint weekends
-def reserve(list, driver_index, fp_index):
-    avg = int(round((list[driver_index][2] + list[driver_index][3] + list[driver_index][4]) / 3, 0))
-    i = 0
-    while i < len(list):
-        for driver_index in list:
-            if driver_index[2] == 0:
-                driver_index[2] = avg
-            if driver_index[3] == 0:
-                driver_index[3] = avg
-            if driver_index[4] == 0:
-                driver_index[4] = avg
-            else: pass
-        i += 1
-
+    return [i[index] for i in drivers]  
+      
 if sprint_wk != "yes":
-    reserve(drivers, 0, 2)
-    points_fp1 = dr_fp1(drivers, 2)
-    points_fp2 = dr_fp1(drivers, 3)
-    points_fp3 = dr_fp1(drivers, 4)
+    points_fp1 = dr_fp1(drivers_2023, 2)
+    points_fp2 = dr_fp1(drivers_2023, 3)
+    points_fp3 = dr_fp1(drivers_2023, 4)
     sumindex = [sum(fp_total) for fp_total in zip(points_fp1, points_fp2, points_fp3)]
 else: 
-    points_fp1 = dr_fp1(drivers, 2)
+    points_fp1 = dr_fp1(drivers_2023, 2)
     sumindex = points_fp1
 
-drivers_names = dr_fp1(drivers, 0)
+drivers_names = dr_fp1(drivers_2023, 0)
 
 for d, s in zip(drivers_names, sumindex):
     print("Driver/points:", s, d)
